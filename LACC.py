4c6c8902950c95c4aee32e6fdbe70601afc41a49
Andrewtsai1105 <101798632+Andrewtsai1105@users.noreply.github.com>
2023-05-12 23:24:17 +0800

    Update LACC.py

--- LACC.py
+++ LACC.py
@@ -7,60 +7,54 @@ def MCLP(img):
   output: color transfer image
   """
   # Convert image to float for calculations
-    img = img.astype(np.float32)
+  img = img.astype(np.float32)
   # Split image into color channels
-    B, G, R = cv2.split(img)
-    mean_B, mean_G, mean_R = np.mean(B), np.mean(G), np.mean(R)
+  B, G, R = cv2.split(img)
+  mean_B, mean_G, mean_R = np.mean(B), np.mean(G), np.mean(R)
     
   # Define the color channels of the largest mean value, medium mean value, and smallest mean value
-    color_means = {"B": mean_B, "G": mean_G, "R": mean_R}
-    sorted_colors = sorted(color_means.items(), key=lambda x: x[1])
-    I_s, I_m, I_l = None, None, None
+  color_means = {"B": mean_B, "G": mean_G, "R": mean_R}
+  sorted_colors = sorted(color_means.items(), key=lambda x: x[1])
+  I_s, I_m, I_l = None, None, None
 
-    for idx, (color, mean) in enumerate(sorted_colors):
-        if idx == 0:  # Smallest mean value
-            if color == "B":
-                I_s = B
-            elif color == "G":
-                I_s = G
-            else:
-                I_s = R
-        elif idx == 1:  # Medium mean value
-            if color == "B":
-                I_m = B
-            elif color == "G":
-                I_m = G
-            else:
-                I_m = R
-        else:  # Largest mean value
-            if color == "B":
-                I_l = B
-            elif color == "G":
-                I_l = G
-            else:
-                I_l = R
-   
-  
+  for idx, (color, mean) in enumerate(sorted_colors):
+      if idx == 0:  # Smallest mean value
+          if color == "B":
+              I_s = B
+          elif color == "G":
+              I_s = G
+          else:
+              I_s = R
+      elif idx == 1:  # Medium mean value
+          if color == "B":
+              I_m = B
+          elif color == "G":
+              I_m = G
+          else:
+              I_m = R
+      else:  # Largest mean value
+          if color == "B":
+              I_l = B
+          elif color == "G":
+              I_l = G
+          else:
+              I_l = R
   # Apply the color correction
-    I_l_cr = 255 * (I_l - np.min(I_l)) / (np.max(I_l) - np.min(I_l))
+  I_l_cr = 255 * (I_l - np.min(I_l)) / (np.max(I_l) - np.min(I_l))
+  
+  while True:
+      I_m_cr = I_m + (np.mean(I_l) - np.mean(I_m)) * I_l
+      I_s_cr = I_s + (np.mean(I_l) - np.mean(I_s)) * I_l
 
-
-
-    while True:
-        I_m_cr = I_m + (np.mean(I_l) - np.mean(I_m)) * I_l
-        I_s_cr = I_s + (np.mean(I_l) - np.mean(I_s)) * I_l
-        
-        # Check for convergence
-        loss = abs(np.mean(I_l) - np.mean(I_m)) + abs(np.mean(I_l) - np.mean(I_s))
-        if loss < 1e-2:
-            break
-
-        I_m, I_s = I_m_cr, I_s_cr
+      # Check for convergence
+      loss = abs(np.mean(I_l) - np.mean(I_m)) + abs(np.mean(I_l) - np.mean(I_s))
+      if loss < 1e-2:
+          break
+      I_m, I_s = I_m_cr, I_s_cr
 
   # Merge the color corrected channels
-    img_ct = cv2.merge([I_s_cr, I_m_cr, I_l_cr]).astype(np.uint8)
-    
-    return img_ct
+  img_ct = cv2.merge([I_s_cr, I_m_cr, I_l_cr]).astype(np.uint8)
+  return img_ct
 
 def MAMGF(img_origin, img_ct):
   """
@@ -68,15 +62,15 @@ def MAMGF(img_origin, img_ct):
   output: color corrected image
   """
   # Convert image to float for calculations
-    img_origin = img_origin.astype(np.float32)
-    img_ct = img_ct.astype(np.float32)
+  img_origin = img_origin.astype(np.float32)
+  img_ct = img_ct.astype(np.float32)
     
-    # Apply the max attenuation map
-    gamma = 1.2
-    A_max_M = np.max([1 - img_origin[..., i] ** gamma for i in range(3)], axis=0)
-    
-    # Apply the color correction
-    D = img_origin - cv2.GaussianBlur(img_origin, (3, 3), 0)
-    I_cc = D + A_max_M[..., None] * img_ct + (1 - A_max_M[..., None]) * img_origin
-    
-    return I_cc.astype(np.uint8)  
+  # Apply the max attenuation map
+  gamma = 1.2
+  A_max_M = np.max([1 - img_origin[..., i] ** gamma for i in range(3)], axis=0)
+
+  # Apply the color correction
+  D = img_origin - cv2.GaussianBlur(img_origin, (3, 3), 0)
+  I_cc = D + A_max_M[..., None] * img_ct + (1 - A_max_M[..., None]) * img_origin
+
+  return I_cc.astype(np.uint8)  
